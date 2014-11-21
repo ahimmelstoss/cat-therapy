99c74f3802ea9182de37b419a4f0dd2d72f853a7
ahimmelstoss <ahimmelstoss@gmail.com>
2014-11-20 21:13:14 -0500

    cats rendering

--- public/js/video.js
+++ public/js/video.js
@@ -1,20 +1,30 @@
-catVideos = [
-  "http://giphy.com/gifs/fAT2Db0j0Mblu/html5",
-  "http://giphy.com/gifs/IR2WwZdgFxk1G/html5",
-  "http://giphy.com/gifs/E2z4Xp9jWyc3C/html5",
-  "http://giphy.com/gifs/kIkwipWRoqeUE/html5",
-  "http://giphy.com/gifs/CHc9dLQVQOAXm/html5",
-  "http://giphy.com/gifs/JwW4oYd3Wjv6E/html5",
-  "http://giphy.com/gifs/54rAlImPzj3WM/html5",
-  "http://giphy.com/gifs/zZMTVkTeEfeEg/html5",
-  "http://giphy.com/gifs/YAxCzfAc7MHPa/html5",
-  "http://giphy.com/gifs/uw5U12mcksoKs/html5",
-  "http://giphy.com/gifs/d9XU9HcvZX7SU/html5",
-  "http://giphy.com/gifs/S1kXC3fX3X3jy/html5",
-  "http://giphy.com/gifs/iwoathoVXUWfC/html5",
-  "http://giphy.com/gifs/TMmR0ZJe3EJi0/html5",
-  "http://giphy.com/gifs/1RgJ939wcJWsU/html5",
-  "http://giphy.com/gifs/nuD6CKrrpkBS8/html5",
-  "http://giphy.com/gifs/Yh39HZShKqJqg/html5",
-  "http://giphy.com/gifs/q2QWrg8nWES2I/html5",
+var catVideos = [
+  "http://media.giphy.com/media/fAT2Db0j0Mblu/giphy.mp4",
+  "http://media.giphy.com/media/IR2WwZdgFxk1G/giphy.mp4",
+  "http://media.giphy.com/media/E2z4Xp9jWyc3C/giphy.mp4",
+  "http://media.giphy.com/media/kIkwipWRoqeUE/giphy.mp4",
+  "http://media.giphy.com/media/CHc9dLQVQOAXm/giphy.mp4",
+  "http://media.giphy.com/media/JwW4oYd3Wjv6E/giphy.mp4",
+  "http://media.giphy.com/media/54rAlImPzj3WM/giphy.mp4",
+  "http://media.giphy.com/media/zZMTVkTeEfeEg/giphy.mp4",
+  "http://media.giphy.com/media/YAxCzfAc7MHPa/giphy.mp4",
+  "http://media.giphy.com/media/uw5U12mcksoKs/giphy.mp4",
+  "http://media.giphy.com/media/d9XU9HcvZX7SU/giphy.mp4",
+  "http://media.giphy.com/media/S1kXC3fX3X3jy/giphy.mp4",
+  "http://media.giphy.com/media/iwoathoVXUWfC/giphy.mp4",
+  "http://media.giphy.com/media/TMmR0ZJe3EJi0/giphy.mp4",
+  "http://media.giphy.com/media/1RgJ939wcJWsU/giphy.mp4",
+  "http://media.giphy.com/media/nuD6CKrrpkBS8/giphy.mp4",
+  "http://media.giphy.com/media/Yh39HZShKqJqg/giphy.mp4",
+  "http://media.giphy.com/media/q2QWrg8nWES2I/giphy.mp4"
 ]
+
+function displayRandomVideo() {
+  var randomIndex = Math.floor((Math.random() * catVideos.length));
+  $("#cat-video").attr("src", catVideos[randomIndex]);
+  $("#video")[0].load();
+}
+
+$(document).ready(function(){
+  displayRandomVideo();
+});
