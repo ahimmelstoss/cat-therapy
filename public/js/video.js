2aff18e6ad959b36ef4a325c4b38aa58c79d1bee
ahimmelstoss <ahimmelstoss@gmail.com>
2014-11-24 20:19:02 -0500

    v1.5 more vids

--- public/js/video.js
+++ public/js/video.js
@@ -15,7 +15,12 @@ var catVideos = [
   "http://media.giphy.com/media/nuD6CKrrpkBS8/giphy.mp4",
   "http://media.giphy.com/media/Yh39HZShKqJqg/giphy.mp4",
   "http://media.giphy.com/media/q2QWrg8nWES2I/giphy.mp4",
-  "http://media.giphy.com/media/ZuxPPPqY7IaOI/giphy.mp4"
+  "http://media.giphy.com/media/ZuxPPPqY7IaOI/giphy.mp4",
+  "http://media.giphy.com/media/yLgHIG7JJXyqk/giphy.mp4",
+  "http://media.giphy.com/media/evH35Q30UW4XS/giphy.mp4",
+  "http://media.giphy.com/media/lvRqhmHtSqigE/giphy.mp4",
+  "http://media.giphy.com/media/b8OB52IWcwzRu/giphy.mp4",
+  "http://media.giphy.com/media/7odHUWPszD0fC/giphy.mp4"
 ]
 
 function displayRandomVideo() {
